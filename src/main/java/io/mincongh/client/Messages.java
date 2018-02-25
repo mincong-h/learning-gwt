e6a080e9f94db86e41dac83fc0b0638ff9d284a9
Mincong HUANG <mincong.h@gmail.com>
2018-02-25 20:09:44 +0100

    Remove unused annotation @Generate
    Annotation `@Generate` is used for generating properties file from
    annotated interface. The file type is determined by the `format`
    argument, and the file name by the optional `fileName` argument.
    However, since generation is not needed here, it's unnecessary to use
    such annotation.

--- src/main/java/io/mincongh/client/Messages.java
+++ src/main/java/io/mincongh/client/Messages.java
@@ -1,9 +1,6 @@
 package io.mincongh.client;
 
-import com.google.gwt.i18n.client.LocalizableResource.Generate;
-
 // IntelliJ: IntelliJ GWT plugin should inspect the properties file
-@Generate(format = "com.google.gwt.i18n.server.PropertyCatalogFactory")
 public interface Messages extends com.google.gwt.i18n.client.Messages {
 
   @DefaultMessage("Enter your name")
