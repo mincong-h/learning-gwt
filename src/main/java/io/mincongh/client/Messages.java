6cbcc3a73679653cf3384c81ae321e40920a9c87
Mincong HUANG <mincong.h@gmail.com>
2018-02-25 11:07:17 +0100

    Advanced Topics: i18n parameterized messages

--- src/main/java/io/mincongh/client/Messages.java
+++ src/main/java/io/mincongh/client/Messages.java
@@ -15,4 +15,14 @@ public interface Messages extends com.google.gwt.i18n.client.Messages {
   @DefaultMessage("Random Data")
   String randomData();
 
+  @DefaultMessage("Name: {0}")
+  String nameLabel(String name);
+
+  @DefaultMessage("There''re {0} items.")
+  @AlternateMessage({
+      "=0", "There''s no item.",
+      "=1", "There''s 1 item."
+  })
+  String itemLabel(@PluralCount int itemSize);
+
 }
--- src/test/java/io/mincongh/client/GwtTestI18n.java
+++ src/test/java/io/mincongh/client/GwtTestI18n.java
@@ -5,6 +5,8 @@ import com.google.gwt.junit.client.GWTTestCase;
 import io.mincongh.shared.R;
 
 /**
+ * Tests internationalization using French as default language.
+ *
  * @author Mincong Huang
  */
 public class GwtTestI18n extends GWTTestCase {
@@ -17,6 +19,18 @@ public class GwtTestI18n extends GWTTestCase {
   public void testPropertiesFileDiscovery() throws Exception {
     Messages messages = GWT.create(Messages.class);
     assertEquals("Ajouter", messages.addButton());
+  }
+
+  public void testParameterizedValue() throws Exception {
+    Messages messages = GWT.create(Messages.class);
+    assertEquals("Nom: Foo", messages.nameLabel("Foo"));
+  }
+
+  public void testParameterizedValues() throws Exception {
+    Messages messages = GWT.create(Messages.class);
+    assertEquals("Il n'y a pas d'élément.", messages.itemLabel(0));
+    assertEquals("Il y a 1 élément.", messages.itemLabel(1));
+    assertEquals("Il y a 2 éléments.", messages.itemLabel(2));
   }
 
 }
